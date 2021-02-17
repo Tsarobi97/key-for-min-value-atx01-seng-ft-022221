84a809ec38fb7721ae5d60a0edbda2836736a936
Robert Obi <robobi97@googlemail.com>
2021-02-17 00:49:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
-
-key_for_min_value.each do |key|
-  if 
-
-
-
-
-#if value == min return key
+  min_value = nil 
+  min_key = nil 
+  name_hash.each do |key, value|
+    if min_value == nil
+      min_value = value
+      min_key = key
+      elsif
+      min_value > nil
+      min_value = value
+      min_key = key
+    end
+  end
+  min_key
+ end 
+end 
