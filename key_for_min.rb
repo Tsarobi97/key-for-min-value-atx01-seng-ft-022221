d34f2f4395d48fd7f5fa3cc82c1d5e3adb16965d
Robert Obi <robobi97@googlemail.com>
2021-02-17 00:50:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 def key_for_min_value(name_hash)
   min_value = nil 
   min_key = nil 
-  name_hash.each do |key, value|
+  name_hash.each do |k, v|
     if min_value == nil
-      min_value = value
-      min_key = key
+      min_value = v
+      min_key = k
       elsif
       min_value > nil
-      min_value = value
-      min_key = key
+      min_value = v
+      min_key = k
     end
   end
   min_key
